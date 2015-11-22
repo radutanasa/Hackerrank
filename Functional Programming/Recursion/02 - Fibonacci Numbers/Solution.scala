007e5805370f29dcce744fa0dc3be0cd7d857584
Radu Tanasa <radu.tanasa@gmail.com>
2015-11-22 22:36:36 +0200

    naive implementation

---
+++ Functional Programming/Recursion/02 - Fibonacci Numbers/Solution.scala
@@ -0,0 +1,10 @@
+object Solution extends App {
+
+	// naive implementation
+	def fibonacci(n: Int): Int =
+		if (n == 1) 0
+		else if (n == 2) 1
+		else fibonacci(n - 1) + fibonacci(n - 2)
+
+	println(fibonacci(5))
+}
